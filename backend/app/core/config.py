340841d8dafa5b8889bc9c0db950ad116186b654
Yashwanth Singh <yashwanthsinghgarandal@gmail.com>
2026-06-18 17:59:29 +0530

    add config secert key

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -2,7 +2,7 @@ from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
     DATABASE_URL:str #postgresql connection string
-    SECRET_KEY:str   # used to sign JWT  tokens
+    SECRET_KEY:str= "claimflow-super-secert-key-2026"  # used to sign JWT  tokens
     ALGORITHM:str = "HS256"   # how JWT is encrypted
     ACCESS_TOKEN_EXPIRE_MINUTES:int = 30
     GROQ_API_KEY:str
